043ea24bb933a1e4f91913e5ba13fab196834991
iikerm <iker.uni05@gmail.com>
2025-07-12 19:00:19 +0200

    Tested a few things

--- src/tests.cpp
+++ src/tests.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <cmath>
 using namespace std;
 
 /*
@@ -8,6 +9,7 @@ to end (both defined). If there is a point that is contained in the bounds of an
 (presumably from a vector of objects), the process will finish.
 */
 
+
 int main(){
     sf::RenderWindow win(sf::VideoMode::getDesktopMode(), "Tests");
     win.setFramerateLimit(120);
@@ -15,6 +17,26 @@ int main(){
     sf::RectangleShape viewLaser(sf::Vector2f(5, win.getSize().y));
     viewLaser.setFillColor(sf::Color(253, 146, 16));       // #fd9210
     viewLaser.setPosition(sf::Vector2f(0, 0));
+
+    sf::RectangleShape diag(sf::Vector2f(1, 350));
+    diag.setFillColor(sf::Color::Red);       // #fd9210
+    diag.setPosition(sf::Vector2f(400, 100));
+
+    sf::RectangleShape diag2(sf::Vector2f(-400, -400));
+    diag2.setPosition(sf::Vector2f(win.getSize().x/2, win.getSize().y/2));
+
+    sf::CircleShape cir(100.f, 50);
+    cir.setFillColor(sf::Color::Red);
+    cir.setPosition(sf::Vector2f(900, 100));
+
+    sf::FloatRect bounds = diag2.getGlobalBounds();
+    sf::RectangleShape drawBounds(bounds.getSize());
+    drawBounds.setPosition(bounds.getPosition());
+    drawBounds.setFillColor(sf::Color::Transparent);
+    drawBounds.setOutlineColor(sf::Color::Blue);
+    drawBounds.setOutlineThickness(3);
+
+    
 
     while (win.isOpen()){
         sf::Event event;
@@ -41,6 +63,10 @@ int main(){
         win.clear(sf::Color::Black);
 
         win.draw(viewLaser);
+        win.draw(cir);
+        win.draw(diag);
+        win.draw(diag2);
+        win.draw(drawBounds);
 
         win.display();
     }
